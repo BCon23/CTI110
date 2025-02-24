4a99a6b9f81a4fd42dd7100d085b3cde3a78ddf7
BCon23 <burksc2178@student.faytechcc.edu>
2025-02-23 21:55:17 -0500

    First Submission
    Submission No 1.

---
+++ P1LAB1_BurksChase.py
@@ -0,0 +1,15 @@
+
+# Chase Burks
+# Date: 2/23/2025
+# P1LAB1
+# User welcome message
+
+
+print('What is your name?:', end='')
+first_name = input()
+print('what is your last name:', end='')
+last_name = input()
+print('Hello', first_name, last_name,'!', 'welcome to CTI-110')
+
+
+
